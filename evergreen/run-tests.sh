8672f9aab598665f97a6221de7819ca8a4ea7b56
BorisDog <boris.dogad@gmail.com>
2024-06-28 13:52:45 -0700

    - Test

--- evergreen/run-tests.sh
+++ evergreen/run-tests.sh
@@ -2,8 +2,12 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-echo Testing with driver: "$DRIVER_VERSION"
+echo Testing with driver: "$DRIVER_VERSION" ${TARGET_FRAMEWORK}
 
 dotnet clean "./MongoDB.Analyzer.sln"
+echo Building
+
 dotnet build "./tests/MongoDB.Analyzer.Tests.Common.ClassLibrary" -f netstandard2.0 -c Debug
+
+echo TESTING 
 dotnet test "./MongoDB.Analyzer.sln" -e DRIVER_VERSION="$DRIVER_VERSION" --framework ${TARGET_FRAMEWORK} -c Release --results-directory ./build/test-results --logger "junit;LogFileName=TEST_MongoDB.Analyzer.xml;FailureBodyFormat=Verbose"
